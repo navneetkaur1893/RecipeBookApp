d724ae443d57f03ebc9ee8559730f5ca7c455095
navneetkaur1893 <72460491+navneetkaur1893@users.noreply.github.com>
2020-10-06 15:15:23 -0500

    Removed extra space.

--- RecepieBook/Features/Controller/InitialViewController.swift
+++ RecepieBook/Features/Controller/InitialViewController.swift
@@ -17,8 +17,6 @@ class InitialViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
-    
-    
     override func viewWillAppear(_ animated: Bool) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             self.performSegue(withIdentifier: "ShowLoginScreen", sender: nil)
